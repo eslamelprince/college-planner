1901ce19916d0730faa32c62aef4c30599773683
Eslam Elprince <eslamelprince@gmail.com>
2021-05-29 15:35:14 -0400

    main menu feature

---
+++ main.py
@@ -0,0 +1,7 @@
+print("1.enter classes")
+print("2.view classes")
+x = input("select a number from the menu: ")
+if x == str(1) :
+    print("Enter classes")
+elif x == str(2):
+    print("View classes")
