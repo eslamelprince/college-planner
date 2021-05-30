ca311a9cc8d1c0ab11e2914e5d69ad61d31763d5
Omar Elprince <oelprince@gmail.com>
2021-05-30 13:57:59 -0400

    going back to main menu

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 from collage import entry_classes
 from collage.Course import Course
 from collage import enter_classes
+while(1==1):
+    print("1.enter classes")
+    print("2.view classes")
+    print("3.exit")
+    x = input("select a number from the menu: ")
+    if x == str(1) :
+        print("Enter classes")
+        course = enter_classes.print_enter_class_menu()
+    elif x == str(2):
+        print("View classes")
+    elif x == str(3):
+        print("goodbye")
+        break
 
-print("1.enter classes")
-print("2.view classes")
-print("3.exit")
-x = input("select a number from the menu: ")
-if x == str(1) :
-    print("Enter classes")
-    course = enter_classes.print_enter_class_menu()
-elif x == str(2):
-    print("View classes")
-elif x == str(3):
-    print("goodbye")
-    exit
+
