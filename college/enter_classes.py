a6b6f602d85605596fd26c09a63d8ebdb2d478e8
Eslam Elprince <eslamelprince@gmail.com>
2021-06-03 10:13:10 -0400

    prereq validation and class print after input

--- college/enter_classes.py
+++ college/enter_classes.py
@@ -1,4 +1,5 @@
 from college.Course import Course
+from store import data
 
 def print_enter_class_menu():
     name = input("name :")
@@ -6,6 +7,7 @@ def print_enter_class_menu():
     course.credit_hours = input("credit hours :")
     while (1==1):
         prepeq = input("prerequests :")
+        data.search_course(prepeq)
         if (prepeq == "x"):
             prepeq = ""
             break
@@ -13,4 +15,3 @@ def print_enter_class_menu():
             course.prerequests.append(prepeq)
     course.location = input("location :")
     return course
-
